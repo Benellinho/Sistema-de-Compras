ce81928f2c89c7dcfea5ec532ba2b86a618494ee
Benellinho <1lucasbenelli@gmail.com>
2026-06-16 20:03:05 -0300

    feat: implementação de alterações dos itens nos lançamentos e melhoria da aceitação das justificativas positivas e negativas

--- backend/src/modules/solicitacoes/itens-solicitacao/itens-solicitacao.routes.js
+++ backend/src/modules/solicitacoes/itens-solicitacao/itens-solicitacao.routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { createItemSolicitacao, deleteItemSolicitacao } from './itens-solicitacao.controller.js';
+import {
+  createItemSolicitacao,
+  deleteItemSolicitacao,
+  updateItemSolicitacao
+} from './itens-solicitacao.controller.js';
 
 // Routes recebem as requisições HTTP e direcionam para o controller correto.
 const router = Router({ mergeParams: true });
 
 router.post('/', createItemSolicitacao);
+router.put('/:itemSolicitacaoId', updateItemSolicitacao);
 router.delete('/:itemSolicitacaoId', deleteItemSolicitacao);
 
 export default router;
